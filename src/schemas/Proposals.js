43f8779dc5aecaeb762be7e34ecabf849b9260c4
Yasoja44 <yasoja44@gmail.com>
2021-06-06 20:39:29 +0530

    sign added

--- src/schemas/Proposals.js
+++ src/schemas/Proposals.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose');
 const ProposalSchema=new mongoose.Schema({
     propo_author:[{ type: mongoose.Schema.Types.ObjectId, required: false, ref: 'Users'}],
     propo_content:{type:String},
-    propo_contact:{type:Number,required:true}
+    propo_contact:{type:Number,required:true},
+    propo_sign:{type:String}
 });
 const Proposal = mongoose.model('Proposal', ProposalSchema);
 module.exports = Proposal;
